a3fcc2aa42f953c7ea9fa7d6cbb5e4167e7faf34
Ali Kiyan <Alikiyand@gmail.com>
2021-01-04 00:39:04 +0000

    finished classed based email validator

--- src/main/java/com/alikn/demo/user/UserService.java
+++ src/main/java/com/alikn/demo/user/UserService.java
@@ -1,5 +1,7 @@
 package com.alikn.demo.user;
 
+import com.alikn.demo.EmailValidator;
+import com.alikn.demo.exception.ApiRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,12 @@ import java.util.UUID;
 @Service
 public class UserService {
     private final userDataAccessService userDataAccessService;
+    private final EmailValidator emailValidator;
 
     @Autowired
-    public UserService(com.alikn.demo.user.userDataAccessService userDataAccessService) {
+    public UserService(com.alikn.demo.user.userDataAccessService userDataAccessService, EmailValidator emailValidator) {
         this.userDataAccessService = userDataAccessService;
+        this.emailValidator = emailValidator;
     }
 
     List<User> getAllUsers() {
@@ -24,6 +28,9 @@ public class UserService {
 
     void addNewUser(UUID userId, User user) {
         UUID newUserId = Optional.ofNullable(userId).orElse(UUID.randomUUID());
+        if(!emailValidator.test(user.getEmail())){
+            throw new ApiRequestException(user.getEmail() + " is not Valid");
+        }
         userDataAccessService.insertUser(newUserId, user);
     }
 

ac9453c88afeef87a277747223193e9f2bca00e2
Ali Kiyan <Alikiyand@gmail.com>
2021-01-04 00:20:31 +0000

    added tests

--- src/test/java/com/alikn/demo/exception/ApiExceptionHandlerTest.java
+++ src/test/java/com/alikn/demo/exception/ApiExceptionHandlerTest.java
@@ -13,4 +13,15 @@ class ApiExceptionHandlerTest {
     public void itShouldValidateCorrectEmail() {
         assertThat(underTest.test("hello@gmail.com")).isTrue();
     }
+
+    @Test
+    public void itShouldValidateAnIncorrectEmail() {
+        assertThat(underTest.test("hello.com")).isFalse();
+    }
+
+    @Test
+    public void itShouldValidateWithoutDotEmail() {
+        assertThat(underTest.test("hello@com")).isFalse();
+    }
+
 }
